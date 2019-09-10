bcf6b5c7a1f1ea73a284feedddd33c18d4ced3f6
ringsaturn <ringsaturn.me@gmail.com>
2019-09-10 14:40:01 +0800

    Release v1.8.1a0
    We release wrong codes on the previous release tag v1.8.1

--- rb/__init__.py
+++ rb/__init__.py
@@ -14,7 +14,7 @@ from rb.router import BaseRouter, ConsistentHashingRouter, PartitionRouter, \
 from rb.promise import Promise
 
 
-__version__ = '1.8.1'
+__version__ = '1.8.1a'
 
 __all__ = [
     # cluster
